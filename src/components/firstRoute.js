ec20eb01e34d1406dd6d4cfbfa382b55935350e4
NajamShehzad <najambutt195@gmail.com>
2018-10-02 22:38:38 +0500

    Setting Files

--- src/components/firstRoute.js
+++ src/components/firstRoute.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 
 
 const firstRoute = (props) => {
-    console.log(props);
     
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/store';
+
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filter';
+import getVisibleExpenses from './selector/expenses';
+
+
+const store = configureStore();
+store.dispatch(addExpense({description:"Study Redux"}));
+store.dispatch(addExpense({description:"Chat App"}));
+store.dispatch(setTextFilter('App'))
+
+console.log(store.getState());
+
+const state  = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+console.log(visibleExpenses);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
